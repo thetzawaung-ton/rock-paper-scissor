5551b6c3c3a03fbb07ad7ac60b9e7b067c0136c1
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-11 14:19:25 +0630

    Add playRound function with two parameter to determine who wins
    Using condions and two parameters for human choice and computer choice, log the message who wins

--- javascript.js
+++ javascript.js
@@ -25,3 +25,46 @@ function getHumanChoice() {
 //CREATE new variables for human and computer score and set initial value to 0//
 let humanScore = 0;
 let ComputerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    //IF humanChoice is rock and computerChoice is paper//
+    if(humanChoice === 'rock' && computerChoice === 'paper') {
+        //DISPLAY human loses//
+        console.log('You lose! Paper beats rock');
+    }
+    //ELSE IF humanChoice is paper and computerChoice is scissors//
+    else if(humanChoice === 'paper' && computerChoice === 'scissors') {
+        //DISPLAY human loses//
+        console.log('You lose! Scissors beats paper');
+    }
+    //ELSE IF humanChoice is scissors and computerChoice is rock//
+    else if(humanChoice === 'scissors' && computerChoice === 'rock') {
+        //DISPLAY human loses//
+        console.log('You lose! Rock beats scissors');
+    }
+    //ELSE IF humanChoice is rock and computerChoice is scissors//
+    else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        //DISPLAY human wins//
+        console.log ('You win! Rock beats scissors');
+    }
+    //ELSE IF humanChoice is sicssors and computerChoice is paper//
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        //DISPLAY human wins//
+        console.log ('You win! Scissors beats paper');
+    }
+    //ELSE IF humanChoice is same as computerChoice//
+    else if (humanChoice === computerChoice) {
+        //DISPLAY as a tie//
+        console.log ('Ooof! It is a tie');
+    }
+    //ELSE//
+    else {
+        //DISPLAY human wins//
+        console.log ('You wins! Paper beats rock');
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
