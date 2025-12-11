0def665ded1458bbd26d36b318527447fa5a952f
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-11 13:28:36 +0630

    Add getComputerChoice function to determine computer choice
    First, created random number from 1 to 3 with Math.random and change those to Rock, Paper, and Scissor using conditions.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log("hello world")
+
+function getComputerChoice() {
+    //GET computer choice with random number generator from 1 to 3//
+    computerChoice = Math.floor((Math.random() * 3) + 1);
+    //IF number is 1, return with the string 'Rock'//
+    //ELSE IF number is 2, return with the string 'Paper'//
+    //ELSE return 'Scissor'//
+    if(computerChoice === 1) {
+        return 'Rock';
+    } else if ( computerChoice === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissor';
+    }
+}
