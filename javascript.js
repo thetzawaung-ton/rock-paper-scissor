fb21c372c8e599f547524f6ed04444bcdca5a88c
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-11 13:30:48 +0630

    Fix typo from scissor to scissors

--- javascript.js
+++ javascript.js
@@ -5,12 +5,12 @@ function getComputerChoice() {
     computerChoice = Math.floor((Math.random() * 3) + 1);
     //IF number is 1, return with the string 'Rock'//
     //ELSE IF number is 2, return with the string 'Paper'//
-    //ELSE return 'Scissor'//
+    //ELSE return 'Scissors'//
     if(computerChoice === 1) {
         return 'Rock';
     } else if ( computerChoice === 2) {
         return 'Paper';
     } else {
-        return 'Scissor';
+        return 'Scissors';
     }
 }
